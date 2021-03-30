0c3136eafe5740bea3afc0df47c09e256b82653d
punkrockviking <danjeccles@gmail.com>
2021-03-30 13:18:00 -0600

    (feat) isTriangle problem solved

---
+++ isTriangle.js
@@ -0,0 +1,18 @@
+// Implement a method that accepts 3 integer values a, b, c. The method should return true if a triangle can be built with the sides of given length and false in any other case.
+
+// (In this case, all triangles must have surface greater than 0 to be accepted).
+
+function isTriangle(a,b,c) {
+    if ((a + b > c) && (b + c > a) && (c + a > b)) {
+      return true
+    } else {
+    return false;
+    }
+  }
+
+
+  //Test Cases
+  Test.describe("PublicTest", function() {
+    Test.assertEquals(isTriangle(1,2,2), true);
+    Test.assertEquals(isTriangle(7,2,2), false);
+});
