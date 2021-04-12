b1efb7a91fee8cedb9dffabb8da16e4e5d82e333
punkrockviking <danjeccles@gmail.com>
2021-04-12 15:40:18 -0600

    (feat) solved problem. used 3 variables to track the different bills individually instead of a single 'total' variable

--- movieClerk.js
+++ movieClerk.js
@@ -8,24 +8,33 @@
 // Return YES, if Vasya can sell a ticket to every person and give change with the bills he has at hand at that moment. Otherwise return NO.
 
 function tickets(peopleInLine) {
-    let total = 25
-    if (peopleInLine[0] !== 25) {
+  if (peopleInLine[0] !== 25) {
+    return 'NO'
+  }
+  let twentyFives, fifties, hundreds
+  twentyFives = 1
+  fifties = 0
+  hundreds = 0
+  for(let i = 1; i < peopleInLine.length; i++) {
+    const currentPerson = peopleInLine[i]
+    if (currentPerson === 25) {
+      twentyFives += 1
+    } else if (currentPerson === 50 && twentyFives >= 1) {
+      fifties += 1
+      twentyFives -= 1
+    } else if (currentPerson === 100 && (twentyFives >= 1 && fifties >= 1)) {
+      hundreds += 1
+      fifties -= 1
+      twentyFives -= 1
+    } else if (currentPerson === 100 && (twentyFives >= 3 && fifties === 0)) {
+      hundreds += 1
+      twentyFives -= 3
+    } else {
       return 'NO'
     }
-    for(let i = 1; i < peopleInLine.length; i++) {
-      const currentPerson = peopleInLine[i]
-      const hasChange = (currentPerson - 25) <= total
-      const exactChange = currentPerson === 25
-      if (exactChange) {
-        total += currentPerson
-      } else if (hasChange) {
-        total = total + currentPerson - (currentPerson - 25)
-      } else {
-        return 'NO'
-      }
-    }
-    return 'YES'
   }
+  return 'YES'
+}
 
 
   //TESTS
