04c010fe60dfe694bdea35e4bf2cab9d65862dab
punkrockviking <danjeccles@gmail.com>
2021-04-15 12:33:12 -0600

    (feat) solved 'string ends with' problem using .length

---
+++ stringEndsWith.js
@@ -0,0 +1,19 @@
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+
+function solution(str, ending){
+    let startOfEnd = str[str.length - ending.length]
+    
+    for(let i = 0; i < ending.length; i++) {
+      const currStrLetter = str[(str.length - ending.length) + i]
+      const currEndLetter = ending[i]
+      if (currStrLetter !== currEndLetter) {
+        return false
+      }
+    }
+    return true  
+  }
+
+
+  Test.assertEquals(solution('abcde', 'cde'), true)
+Test.assertEquals(solution('abcde', 'abc'), false)
