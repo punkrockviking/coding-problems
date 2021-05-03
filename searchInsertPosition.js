05d344e3a190c77b42f1b525b4c3b42bd34e021d
punkrockviking <danjeccles@gmail.com>
2021-05-03 14:36:13 -0600

    (feat) solved using brute force

---
+++ searchInsertPosition.js
@@ -0,0 +1,18 @@
+/*
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    const objNums = Object.assign({}, nums);
+    
+    if ( target > nums[nums.length -1]) {
+        return nums.length
+    }
+    for (let i = 0; i < nums.length; i++) {
+     const curr = nums[i]
+     if (curr === target || curr > target) {
+         return i
+     }
+    }
+};
