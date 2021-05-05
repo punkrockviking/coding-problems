772b0b0a36c584a6fabda4d20f7c00445e433b45
punkrockviking <danjeccles@gmail.com>
2021-05-05 13:05:02 -0600

    (feat) solved, uses multiple loops, not ideal.

---
+++ isStringPalindrome.js
@@ -0,0 +1,32 @@
+/*
+Given a string s, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    let palindrome = true
+    // make all lowercase
+    let sentence = s.toLowerCase()
+    // remove spaces and punctuation from string
+    const regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g
+    sentence = sentence.replace(regex, '')
+    for (i = 0; i < sentence.length; i++) {
+      if (sentence[i] === ' ') {
+        sentence = sentence.replace(sentence[i],'')
+        i --
+      }
+    }
+    // compare outsides and work way into middle
+    let left = 0
+    let right = sentence.length - 1
+    while (left < right) {
+      if (sentence[left] !== sentence[right]) {
+        palindrome = false
+        return palindrome
+      } else if (sentence[left] === sentence[right]) {
+        left += 1
+        right -= 1
+      }
+    }
+    return palindrome
+  };
