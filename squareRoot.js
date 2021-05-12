a91a7cc89230d33df4067f7e1db7d90ff4f48026
punkrockviking <danjeccles@gmail.com>
2021-05-11 22:35:26 -0600

    (feat) solved using Math.floor and Math.sqrt

---
+++ squareRoot.js
@@ -0,0 +1,11 @@
+/*
+Given a non-negative integer x, compute and return the square root of x.
+
+Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
+ 
+* @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    return Math.floor(Math.sqrt(x))
+};
