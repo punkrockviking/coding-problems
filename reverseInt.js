bed34de954b416ca95919b5aff8be137339b2936
punkrockviking <danjeccles@gmail.com>
2021-05-13 22:14:50 -0600

    (feat) started, helper function to flip array

---
+++ reverseInt.js
@@ -0,0 +1,19 @@
+var reverse = function(x) {
+    const flip = function(x){
+      const str = x.toString()
+      const arr = str.split('')
+      const flippedArr = []
+      for (let i = arr.length - 1 i >= 0; i--){
+        flippedArr.push(arr[i])
+      }
+    }
+    if (x === 0) {
+      return 0
+    }
+    if (x < 0) {
+  
+    }
+  };
+  
+  reverse(123)
+  reverse(-123)
