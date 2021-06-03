a5ebf0ddad6f7e0ff88aef1da943c9d5a56efc4b
punkrockviking <danjeccles@gmail.com>
2021-06-02 22:52:57 -0600

    (feat) solved without strings

--- reverseInt.js
+++ reverseInt.js
@@ -1,19 +1,30 @@
-var reverse = function(x) {
-    const flip = function(x){
-      const str = x.toString()
-      const arr = str.split('')
-      const flippedArr = []
-      for (let i = arr.length - 1 i >= 0; i--){
-        flippedArr.push(arr[i])
-      }
-    }
-    if (x === 0) {
-      return 0
-    }
-    if (x < 0) {
-  
-    }
-  };
-  
-  reverse(123)
-  reverse(-123)
+// Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+// Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+
+const reverse = function(x) {
+  const isNegative = x< 0 ? true : false;
+
+if (isNegative){
+    x = x *-1;
+}
+
+let reversed = 0;
+while(x>0){
+    reversed = (reversed * 10) + (x % 10);
+    
+    x = parseInt(x/10);
+}
+
+if(reversed > 2**31){
+    return 0;
+}
+
+return isNegative? reversed * -1 : reversed;
+};
+
+reverse(123)
+reverse(-123)
+reverse(120)
+reverse(0)
