e09abdd43bfcc9cfbcd99a0bf0a496d227d053d5
Dan Eccles <79684850+punkrockviking@users.noreply.github.com>
2021-07-30 04:47:03 +0000

    (feat) in order traversal solved iterative and recursive

---
+++ binaryTreeInorderTraversal.js
@@ -0,0 +1,96 @@
+/*
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+Example 1:
+
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+Example 4:
+
+
+Input: root = [1,2]
+Output: [2,1]
+Example 5:
+
+
+Input: root = [1,null,2]
+Output: [1,2]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+ 
+
+Follow up: Recursive solution is trivial, could you do it iteratively?
+*/
+
+
+class Node {
+  constructor(val=null, left=null, right=null) {
+    this.val = val
+    this.left = left
+    this.right = right
+  }
+}
+
+
+// Input: root = [1,null,2,3]
+
+
+
+const node3 = new Node(3, null, null)
+const node2 = new Node(2, node3)
+const node1 = new Node(1, null, node2)
+
+
+
+
+const inorderTraversalIterative = function(root) {
+  const output = []
+  const stack = []
+  let currentNode = root 
+  while (currentNode || stack.length) {
+    if (currentNode) {
+      stack.push(currentNode)
+      currentNode = currentNode.left
+    } else {
+      currentNode = stack.pop()
+      output.push(currentNode.val)
+      currentNode = currentNode.right
+    }
+
+
+  }
+  return output
+}
+const inorderTraversalRecursive = function(root) {
+  const output = []
+  const traverse = function(node) {
+    if (node === null) {
+      return
+    }
+    //move to left
+    traverse(node.left)
+    output.push(node.val)
+    // push value onto results array
+    // check right
+    traverse(node.right)
+    return
+  }
+
+  traverse(root)
+  return output
+};
+
+inorderTraversalIterative(node1)
+inorderTraversalRecursive(node1)
