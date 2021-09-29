77145d9805b02f646a827d7e5a5f79617ea31921
Dan Eccles <79684850+punkrockviking@users.noreply.github.com>
2021-09-29 17:46:38 +0000

    (feat) solved mergeSortedLists problem

--- mergeSortedLists.js
+++ mergeSortedLists.js
@@ -4,10 +4,36 @@ Merge two sorted linked lists and return it as a sorted list. The list should be
 
 class List {
   constructor(val, next=null) {
-  this.val = val
-  this.next = next  
-}
+    this.val = val
+    this.next = next  
+  }
 } 
+
+
+const mergeTwoLists = function(l1, l2) {
+  // create merged merged list, list 1, and list 2
+  let cur1 = l1
+  let cur2 = l2
+  let mergedList = new List(null)
+  let head = mergedList
+  
+  while (cur1 && cur2) {
+    // compare current node of list 1 and 2
+    if (cur1.val <= cur2.val) {
+      // set current node.val of merged list equal to the < val
+      mergedList.next = new List(cur1.val)
+      // set the val of the list we chose equal to its .next property
+      cur1 = cur1.next
+    } else {
+      mergedList.next = new List(cur2.val)
+      cur2 = cur2.next
+    }
+    mergedList = mergedList.next
+  }
+  mergedList.next = cur1 ? cur1 : cur2
+
+  return head.next    
+};
 
 //1,2,4
 //1,3,4
@@ -22,25 +48,4 @@ const bb = new List(3)
 const cc = new List(4)
 aa.next = bb
 bb.next = cc
-
-const mergeTwoLists = function(l1, l2) {
-  // create merged merged list, list 1, and list 2
-  const cur1 = l1
-  const cur2 = l2
-  const mergedList = new List(null)
-  const isList1Less = cur1.val <= cur2.val
-  
-  // compare current node of list 1 and 2
-  while (cur1.val && cur2.val) {
-    if (isList1less){
-      
-    }
-  }
-
-  // set current node.val of merged list equal to the < val
-
-  // set the val of the list we chose equal to its .next property
-    
-};
-
-// mergeTwoLists(a,aa)
+mergeTwoLists(a,aa)
