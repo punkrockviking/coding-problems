1db25b322587f362ec25c117d55a1468650d83f9
Dan Eccles <79684850+punkrockviking@users.noreply.github.com>
2022-01-21 04:28:55 +0000

    solved using brute force (nested for loops)

---
+++ maxArea.js
@@ -0,0 +1,46 @@
+/*
+You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+
+Notice that you may not slant the container.
+*/
+
+/*
+ * @param {number[]} height
+ * @return {number}
+ */
+
+// input: array (heights)
+// output: int (max area)
+// constraints: 
+  /* n == height.length
+  2 <= n <= 105
+  0 <= height[i] <= 104
+  */
+const testHeights = [1,8,6,2,5,4,8,3,7]
+const expectedValue = 49
+
+// function maxArea(heights) {
+//   let maxAreaBox = 0
+//   for (let i = 0; i < heights.length; i++) {
+//     const leftHeight = heights[i]
+//     for (let j = i + 1; j < heights.length; j++) {
+//       const rightHeight = heights[j]
+//       console.log('*********************************')
+//       console.log('Curr leftHeight is', leftHeight)
+//       console.log('Curr rightHeight is', rightHeight)
+//       const limitingHeight = Math.min(leftHeight, rightHeight)
+//       const currArea = limitingHeight * (j - i)
+//       console.log('Calculating area using', limitingHeight, 'X', j-i)
+//       console.log('currArea is', currArea)
+//       maxAreaBox = Math.max(maxAreaBox, currArea)
+//       // console.log('maxAreaBox is', maxAreaBox)
+//     }
+//   }
+//   return maxAreaBox
+// };
+
+maxArea(testHeights)
