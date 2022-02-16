78bc10e7975760a8c3692889842fa1661f422665
Dan Eccles <79684850+punkrockviking@users.noreply.github.com>
2022-02-16 06:06:00 +0000

    (feat) solved. sort twice. by start points and by endpoints.

--- meetingRooms2.js
+++ meetingRooms2.js
@@ -1,3 +1,4 @@
+
 /* 
 Given an array of meeting time intervals intervals where intervals[i] = [starti, endi], return the minimum number of conference rooms required.
 
@@ -13,45 +14,35 @@ Input: intervals = [[7,10],[2,4]]
 Output: 1
 */
 
-// how do i know if there is overlap?
+// sort by startpoints
+// sort by endpoints
+// compare start of startpoints to end of endpoints
+// if < add to rooms otherwise advance down endpoints
+// advance on startpoints
 
-// input intervals
-// compare current meeting to next meeting
-// sort meetings by start time
-// is there overlap?
-const isOverlap = (int1, int2) => {
-  // const start1 = int1[0]
-  const end1 = int1[1]
-  const start2 = int2[0]
-  // const end2 = int2[1]
-  return start2 < end1
-}
-// while there is overlap continue iterating
-// when there is no overlap, subtract 1 from max rooms and move on to next interval
-// loop
-
-// business
-// output number of rooms needed
-
-const minMeetingRoom = (intervals) => {
-  let rooms = intervals.length
-  intervals.sort((a,b) => a[0] - b[0])
-  for (let i = 0; i < intervals.length - 1; i++) {
-    const currInt = intervals[i]
-    for(let j = i + 1; j < intervals.length; j++) {
-      // my next i is completely within my current i
-      if (!isOverlap(intervals[i], intervals[j])) {
-        if (intervals[j][1] > intervals[i][1]) {
-          rooms --
-        }
-        break
-      }
+const minMeetingRooms = (intervals) => {
+  let rooms = 0
+  const starts = intervals.slice() 
+  starts.sort((a,b) => a[0] - b[0])
+  const ends = intervals
+  ends.sort((a,b) => a[1] - b[1])
+  // console.log(starts)
+  // console.log(ends)
+  let j = 0
+  for (let i = 0; i < starts.length; i++) {
+    const smallestStart = starts[i][0]
+    const smallestEnd = ends[j][1]
+    if (smallestStart < smallestEnd) {
+      rooms ++
+    } else {
+      j++
     }
   }
+
   return rooms
 }
 
 
-const input = [[9,10],[4,9],[4,17]]
+const input = [[0,30],[5,10],[15,20]]
 
 console.log(minMeetingRoom(input))
