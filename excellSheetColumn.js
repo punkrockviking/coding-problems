c0c77b5327ff45f92cb7a6a2a98a45f3506dffe9
Dan Eccles <79684850+punkrockviking@users.noreply.github.com>
2022-12-06 05:22:17 +0000

    still broken. some values are just 1 off. some do not work

--- excellSheetColumn.js
+++ excellSheetColumn.js
@@ -4,18 +4,26 @@
  */
 const convertToTitle = function(columnNumber) {
   let title = ''
+  let rem
   let num = columnNumber
   const alpha = Array.from(Array(26)).map((e, i) => i + 65);
   const alphabet = alpha.map((x) => String.fromCharCode(x));
   // alphabet is an array with each letter as an index value
-  alphabet.unshift('')
+  alphabet.unshift('Z')
   // add an '' to array so each index is equal to the number representation
   
   // see how many times the number is divisible by 26
   while(num > 26) {
-    title += alphabet[Math.floor(num / 26)]
+    div = Math.floor(num / 26)
+    if (div > 26) {
+      title = alphabet[div % 26] + title
+      num = div - 26
+    } else {
+      title = alphabet[div] + title
+      num = num % 26
+      num = num === 0 ? 26 : num
+    }
     console.log(title)
-    num = num % 26
   }
   // use remainder for last digit letter
   title += alphabet[num]
@@ -23,4 +31,4 @@ const convertToTitle = function(columnNumber) {
   return title
 };
 
-console.log(convertToTitle(30))
+console.log(convertToTitle(2147483647))
