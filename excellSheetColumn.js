e49c8aacb5fafa95e798ab3b5c24c7e98d389dda
Dan Eccles <79684850+punkrockviking@users.noreply.github.com>
2022-12-06 02:45:46 +0000

    feat: solved for cases up to 'ZZ'. does not work for larger nums

--- excellSheetColumn.js
+++ excellSheetColumn.js
@@ -2,8 +2,9 @@
  * @param {number} columnNumber
  * @return {string}
  */
-const convertToTitle = function(num) {
+const convertToTitle = function(columnNumber) {
   let title = ''
+  let num = columnNumber
   const alpha = Array.from(Array(26)).map((e, i) => i + 65);
   const alphabet = alpha.map((x) => String.fromCharCode(x));
   // alphabet is an array with each letter as an index value
@@ -11,11 +12,15 @@ const convertToTitle = function(num) {
   // add an '' to array so each index is equal to the number representation
   
   // see how many times the number is divisible by 26
-  
+  while(num > 26) {
+    title += alphabet[Math.floor(num / 26)]
+    console.log(title)
+    num = num % 26
+  }
   // use remainder for last digit letter
   title += alphabet[num]
 
   return title
 };
 
-console.log(convertToTitle(5))
+console.log(convertToTitle(30))
