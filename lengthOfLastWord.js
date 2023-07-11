c9ce958f927b3f15dbf34954c6c7245a47ca915b
punkrockviking <79684850+punkrockviking@users.noreply.github.com>
2023-07-11 03:15:52 +0000

    lengthOfLastWord more efficient solution, starts at end of string and works back til the first space that comes before a word

--- lengthOfLastWord.js
+++ lengthOfLastWord.js
@@ -8,14 +8,31 @@ A word is a maximal substring consisting of non-space characters only.
  * @param {string} s
  * @return {number}
  */
-var lengthOfLastWord = function(s) {
-    let length = 0
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] === ' ' && s[i+1] !== ' ' && !!s[i+1]) {
-            length = 0
-        } else if (s[i] !== ' ') {
-            length ++
-        }
+// var lengthOfLastWord = function(s) {
+//     let length = 0
+//     for (let i = 0; i < s.length; i++) {
+//         if (s[i] === ' ' && s[i+1] !== ' ' && !!s[i+1]) {
+//             length = 0
+//         } else if (s[i] !== ' ') {
+//             length ++
+//         }
+//     }
+//     return length
+// };
+
+const lengthOfLastWord = function(str) {
+  if (str.length === 0) {
+    return 0
+  }
+  let length = 0
+  for (let i = str.length - 1; i >= 0; i--) {
+    if (str[i] === ' ' && length !== 0) {
+      return length
     }
-    return length
-};
+    if (str[i] !== ' ') {
+      length++  
+    }
+  }
+  return length
+}
+
